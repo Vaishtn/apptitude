cf72cfb0c947313277a8962725774dc1835fc172
Vaishtn <73684741+Vaishtn@users.noreply.github.com>
2021-01-24 19:30:18 +0530

    expection

---
+++ expection/src/com/xworkz/expection/EventTester.java
@@ -0,0 +1,14 @@
+package com.xworkz.expection;
+import com.xworkz.expection.ExpectionEvent;
+
+public class EventTester {
+	public static void main(String[] args)throws Throwable {
+		
+		ExpectionEvent event = new ExpectionEvent();
+		
+		event.frame1();
+		
+		
+	}
+
+}
